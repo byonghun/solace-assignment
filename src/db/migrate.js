ad7d737e9267d996faac8d930bf44a7e31184226
Byonghun Kwak <byonghunkwak@gmail.com>
2025-08-17 16:33:09 -0700

    Initial backend fixes

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,11 +1,11 @@
-const { drizzle } = require("drizzle-orm/postgres-js");
-const { migrate } = require("drizzle-orm/postgres-js/migrator");
-const postgres = require("postgres");
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const runMigration = async () => {
   if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-
-  console.log(process.env.DATABASE_URL);
 
   const sql = postgres(process.env.DATABASE_URL, { max: 1 });
   const db = drizzle(sql);
